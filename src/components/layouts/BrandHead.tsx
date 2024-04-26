c7b616e498c532221711e41473401508416da81d
thanhdanh27600 <thanhdanh27600@gmail.com>
2024-04-26 09:23:47 +0700

    update contents

--- src/components/layouts/BrandHead.tsx
+++ src/components/layouts/BrandHead.tsx
@@ -15,7 +15,7 @@ export const BrandHead = ({ feature = FeatureTabKey.SHARE_LINK }: { feature?: Fe
       : feature === FeatureTabKey.SHARE_TEXT
       ? t('brandTitleNote')
       : t('brandTitle')
-    : 'Quickshare | 404 Not Found';
+    : '404 Not Found';
   const brandDescription = locale ? t('brandDescription') : '404 Not Found';
 
   return (
