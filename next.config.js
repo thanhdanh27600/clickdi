a38a78227118ae5ce2bc6ededc4e2ea603f29483
thanhdanh27600 <thanhdanh27600@gmail.com>
2024-06-06 08:24:27 +0700

    try cron

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const { queuePlatform } = require('./src/services/queue/utils');
+const { runQueue } = require('./src/services/queue');
 const { i18n } = require('./next-i18next.config');
 const { cronJob } = require('./src/services/crons');
-const { queueReceiver } = require('./src/services/queue/azure');
-const { sendMessageToRabbitQueue, consumerMessagesRabbit } = require('./src/services/queue/rabbit');
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_SERVER } = require('next/constants');
 const isProduction = process.env.NEXT_PUBLIC_BUILD_ENV === 'production';
 
@@ -13,25 +11,19 @@ const nextConfig = {
   i18n,
 };
 
+
 module.exports = async (phase, { defaultConfig }) => {
-  console.log('Quickshare is starting...');
+  console.log('Quickshare is starting...', phase);
   let shouldRunQueue = phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_SERVER;
   // if (!isProduction) shouldRunQueue = false;
   if (process.env.NEXT_PUBLIC_SHORT_DOMAIN === 'true') shouldRunQueue = false;
   if (shouldRunQueue) {
-    console.log('queuePlatform', queuePlatform);
-    switch (queuePlatform) {
-      case 'AZURE':
-        queueReceiver()
-        break;
-      case 'RABBIT':
-      default:
-        sendMessageToRabbitQueue({ subject: 'health', body: 'Queue is starting...' });
-        consumerMessagesRabbit();
-        break;
-    }
-
+    runQueue()
   }
-  // cronJob();
+  let shouldRunCron = phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_SERVER;
+  if (shouldRunCron) {
+    cronJob();
+  }
+
   return nextConfig;
 };
--- src/services/crons/index.js
+++ src/services/crons/index.js
@@ -1,12 +1,16 @@
 const cron = require('node-cron');
+const { processTasks } = require('../queue/postProcessForward');
 
 function cronJob() {
+  if (process.env.cronJon === 'true') return;
+  process.env.cronJon = 'true';
+
   console.log('Job started at: ' + new Date());
 
-  // // Schedule the job to run every 1 minutes
-  // cron.schedule('*/1 * * * *', () => {
-  //   jobEvery1Minutes();
-  // });
+  // Schedule the job to run every 1 minutes
+  cron.schedule('*/1 * * * *', () => {
+    jobEvery1Minutes();
+  });
   // // Schedule the job to run every 5 minutes
   // cron.schedule('*/5 * * * *', () => {
   //   jobEvery5Minutes();
@@ -15,6 +19,7 @@ function cronJob() {
 
 function jobEvery1Minutes() {
   console.log(`jobEvery1Minutes - ${new Date()}`);
+  processTasks()
 }
 
 function jobEvery5Minutes() {
